1e2f13870d5d0748f85fd97494ca877bbc4879d0
mnzlucas <mnzlucas7@gmail.com>
2021-10-21 20:05:37 -0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input ,Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri"
+import { Flex, Text, Input, Icon, HStack , Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from "react-icons/ri"
 export function Header() {
   return (
     <Flex
@@ -35,16 +35,44 @@ export function Header() {
         bg="gray.800"
         borderRadius="full"
       >
-        <Input 
+        <Input
           color="gray.50"
           variant="unstyled"
           px="4"
           mr="4"
           placeholder="Search"
-          _placeholder={{color: 'gray.400'}}
+          _placeholder={{ color: 'gray.400' }}
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center" >
+          <Box mr="4" textAlign="right" >
+            <Text>Lucas Almeida</Text>
+            <Text color="gray.300" fontSize="small">
+              lucal.almeida@fatec.sp.gov.br
+            </Text>
+          </Box>
+          <Avatar size="md" name="Lucas" src="https://github.com/mnzlucas.png" />
+        </Flex>
       </Flex>
     </Flex>
   )
