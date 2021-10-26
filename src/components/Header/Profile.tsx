0a701335a08bb3d08ac2428edbec9ec542417e85
mnzlucas <mnzlucas7@gmail.com>
2021-10-26 19:03:44 -0300

    Header responsivity

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,19 @@
-import { Flex, Text , Box, Avatar } from "@chakra-ui/react";
+import { Flex, Text, Box, Avatar } from "@chakra-ui/react";
 
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
-export function Profile() {
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center" >
-      <Box mr="4" textAlign="right" >
+      { showProfileData && (<Box mr="4" textAlign="right" >
         <Text>Lucas Almeida</Text>
         <Text color="gray.300" fontSize="small">
           lucal.almeida@fatec.sp.gov.br
         </Text>
       </Box>
+      )}
       <Avatar size="md" name="Lucas" src="https://github.com/mnzlucas.png" />
     </Flex>
   );
