0d9af35e64a0242f0404099ed32d1a261e56b097
mnzlucas <mnzlucas7@gmail.com>
2021-10-28 21:00:10 -0300

    autentication form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Flex, Button, Stack} from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
+  //const {errors} = formstate
   return (
     <Flex
       w="100vw"
@@ -17,18 +31,21 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4" >
           <Input 
               name="email"
               type="email"
               label="E-mail"
+              {...register('email')}
             />
           
           <Input 
               name="password"
               type="password"
               label="Password"
+              {...register('email')}
             />
           
         </Stack>
@@ -38,6 +55,7 @@ export default function SignIn() {
           mt="6"
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >
           Sign in </Button>
       </Flex>
